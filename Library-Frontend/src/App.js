0cc1b98d2d85d0a67cb5bf7b10807fe56aad1bab
Max Mynter <maxmynter@me.com>
2023-02-15 14:09:33 +0100

    Ex. 8.18

--- Library-Frontend/src/App.js
+++ Library-Frontend/src/App.js
@@ -2,23 +2,46 @@ import { useState } from "react";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import Login from "./components/Login";
 
 const App = () => {
   const [page, setPage] = useState("authors");
+  const [loggedIn, setLoggedIn] = useState(false);
 
   return (
     <div>
       <div>
         <button onClick={() => setPage("authors")}>authors</button>
         <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
+        {loggedIn ? (
+          <>
+            <button onClick={() => setPage("add")}>add book</button>
+            <button
+              onClick={() => {
+                setPage("authors");
+                localStorage.clear();
+                setLoggedIn(false);
+              }}
+            >
+              logout
+            </button>
+          </>
+        ) : (
+          <button onClick={() => setPage("login")}>login</button>
+        )}
       </div>
 
       <Authors show={page === "authors"} />
 
       <Books show={page === "books"} />
-
       <NewBook show={page === "add"} />
+      <Login
+        show={page === "login"}
+        setLoggedInTrue={() => {
+          setLoggedIn(true);
+          setPage("authors");
+        }}
+      />
     </div>
   );
 };
---
+++ Library-Frontend/src/components/Login.js
@@ -0,0 +1,69 @@
+import { useState, useEffect } from "react";
+import { useMutation, gql } from "@apollo/client";
+
+const LOGIN = gql`
+  mutation Login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      value
+    }
+  }
+`;
+
+const Login = (props) => {
+  const [message, setMessage] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [login, result] = useMutation(LOGIN, {
+    onError: async (error) => {
+      setMessage(error.graphQLErrors[0].message);
+      setTimeout(() => setMessage(null), 2000);
+    },
+  });
+
+  useEffect(() => {
+    if (result.data) {
+      const token = result.data.login.value;
+      localStorage.setItem("user-token", token);
+      props.setLoggedInTrue();
+    }
+  }, [result.data]); // eslint-disable-line
+
+  if (!props.show) {
+    return null;
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    await login({ variables: { username, password } });
+    setUsername("");
+    setPassword("");
+  };
+
+  return (
+    <div>
+      {message ? <h2 style={{ color: "red" }}>{message}</h2> : null}
+      <form onSubmit={handleLogin}>
+        <div>
+          name{" "}
+          <input
+            onChange={({ target }) => setUsername(target.value)}
+            value={username}
+          />
+        </div>
+
+        <div>
+          password{" "}
+          <input
+            type="password"
+            onChange={({ target }) => setPassword(target.value)}
+            value={password}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
